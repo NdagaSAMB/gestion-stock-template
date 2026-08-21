c8111609864f55db72c2ee43d70b9708245a51e3
ahakim <sambabdouhakim@gmail.com>
2026-08-21 15:34:48 +0000

    traduction fr

--- addbrand.php
+++ addbrand.php
@@ -1,11 +1,11 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) {
-    session_start();
+  session_start();
 }
 // print_r($_SESSION);
 ?>
 <?php if (isset($_SESSION['admin'])): ?>
-<?php
+  <?php
   require_once(__DIR__ . "/php/Class/Marque.php");
   $active = array(0, 0, "active", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
   if (isset($_POST['add'])) {
@@ -18,115 +18,115 @@ if (session_status() === PHP_SESSION_NONE) {
       $brand = new Marque($nom_marque, $description_marque, $image);
       $brand->inserMarque();
     } else {
-      exit("<h3> Failed to upload image!</h3>");
+      exit("<h3>Échec de l'envoi de l'image !</h3>");
     }
   }
-?>
-<!DOCTYPE html>
-<html lang="en">
+  ?>
+  <!DOCTYPE html>
+  <html lang="fr">
 
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
-  <meta name="description" content="POS - Bootstrap Admin Template" />
-  <meta name="keywords"
-    content="admin, estimates, bootstrap, business, corporate, creative, invoice, html5, responsive, Projects" />
-  <meta name="author" content="Dreamguys - Bootstrap Admin Template" />
-  <meta name="robots" content="noindex, nofollow" />
-  <title>Add Brand</title>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
+    <meta name="description" content="POS - Modèle d'administration Bootstrap" />
+    <meta name="keywords"
+      content="admin, estimations, bootstrap, business, corporate, creative, facture, html5, responsive, Projets" />
+    <meta name="author" content="Dreamguys - Modèle d'administration Bootstrap" />
+    <meta name="robots" content="noindex, nofollow" />
+    <title>Ajouter une Marque</title>
 
-  <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png" />
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png" />
 
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
 
-  <link rel="stylesheet" href="assets/css/animate.css" />
+    <link rel="stylesheet" href="assets/css/animate.css" />
 
-  <link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css" />
+    <link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css" />
 
-  <link rel="stylesheet" href="assets/css/dataTables.bootstrap4.min.css" />
+    <link rel="stylesheet" href="assets/css/dataTables.bootstrap4.min.css" />
 
-  <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css" />
-  <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css" />
+    <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css" />
+    <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css" />
 
-  <link rel="stylesheet" href="assets/css/style.css" />
-</head>
+    <link rel="stylesheet" href="assets/css/style.css" />
+  </head>
 
-<body>
-  <div id="global-loader">
-    <div class="whirly-loader"></div>
-  </div>
+  <body>
+    <div id="global-loader">
+      <div class="whirly-loader"></div>
+    </div>
 
-  <div class="main-wrapper">
+    <div class="main-wrapper">
 
-    <?php require_once("header.php"); ?>
-    <?php require_once("sidebar.php"); ?>
+      <?php require_once("header.php"); ?>
+      <?php require_once("sidebar.php"); ?>
 
-    <div class="page-wrapper">
-      <div class="content">
-        <div class="page-header">
-          <div class="page-title">
-            <h4>Add Brand</h4>
-            <h6>Add New Brand</h6>
+      <div class="page-wrapper">
+        <div class="content">
+          <div class="page-header">
+            <div class="page-title">
+              <h4>Ajouter une Marque</h4>
+              <h6>Ajouter une nouvelle marque</h6>
+            </div>
           </div>
-        </div>
-        <div class="card">
-          <div class="card-body">
-            <form class="row" method="post" action="addbrand.php" enctype="multipart/form-data">
-              <div class="col-lg-3 col-sm-6 col-12">
-                <div class="form-group">
-                  <label>Brand Name</label>
-                  <input type="text" name="nom_marque" />
+          <div class="card">
+            <div class="card-body">
+              <form class="row" method="post" action="addbrand.php" enctype="multipart/form-data">
+                <div class="col-lg-3 col-sm-6 col-12">
+                  <div class="form-group">
+                    <label>Nom de la marque</label>
+                    <input type="text" name="nom_marque" />
+                  </div>
                 </div>
-              </div>
-              <div class="col-lg-12">
-                <div class="form-group">
-                  <label>Description</label>
-                  <textarea class="form-control" name="description_marque"></textarea>
+                <div class="col-lg-12">
+                  <div class="form-group">
+                    <label>Description</label>
+                    <textarea class="form-control" name="description_marque"></textarea>
+                  </div>
                 </div>
-              </div>
-              <div class="col-lg-12">
-                <div class="form-group">
-                  <label> Product Image</label>
-                  <div class="image-upload">
-                    <input type="file" name="image" accept="image/png, image/jpeg" />
-                    <div class="image-uploads">
-                      <img src="assets/img/icons/upload.svg" alt="img" />
-                      <h4>Drag and drop a file to upload</h4>
+                <div class="col-lg-12">
+                  <div class="form-group">
+                    <label>Image du produit</label>
+                    <div class="image-upload">
+                      <input type="file" name="image" accept="image/png, image/jpeg" />
+                      <div class="image-uploads">
+                        <img src="assets/img/icons/upload.svg" alt="img" />
+                        <h4>Glissez-déposez un fichier à téléverser</h4>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div class="col-lg-12">
-                <button class="btn btn-submit me-2" type="submit" name="add">Add</button>
-                <a href="brandlist.php" class="btn btn-cancel" type="reset">Cancel</a>
-              </div>
-            </form>
+                <div class="col-lg-12">
+                  <button class="btn btn-submit me-2" type="submit" name="add">Ajouter</button>
+                  <a href="brandlist.php" class="btn btn-cancel" type="reset">Annuler</a>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
 
-  <script src="assets/js/feather.min.js"></script>
+    <script src="assets/js/feather.min.js"></script>
 
-  <script src="assets/js/jquery.slimscroll.min.js"></script>
+    <script src="assets/js/jquery.slimscroll.min.js"></script>
 
-  <script src="assets/js/jquery.dataTables.min.js"></script>
-  <script src="assets/js/dataTables.bootstrap4.min.js"></script>
+    <script src="assets/js/jquery.dataTables.min.js"></script>
+    <script src="assets/js/dataTables.bootstrap4.min.js"></script>
 
-  <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
 
-  <script src="assets/plugins/select2/js/select2.min.js"></script>
+    <script src="assets/plugins/select2/js/select2.min.js"></script>
 
-  <script src="assets/plugins/sweetalert/sweetalert2.all.min.js"></script>
-  <script src="assets/plugins/sweetalert/sweetalerts.min.js"></script>
+    <script src="assets/plugins/sweetalert/sweetalert2.all.min.js"></script>
+    <script src="assets/plugins/sweetalert/sweetalerts.min.js"></script>
 
-  <script src="assets/js/script.js"></script>
-</body>
+    <script src="assets/js/script.js"></script>
+  </body>
 
-</html>
+  </html>
 <?php else: ?>
-<?php header("Location: signin.php"); ?>
+  <?php header("Location: signin.php"); ?>
 <?php endif ?>
