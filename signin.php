8bb370de3b4d260b4f1fe48766ef45b834d30052
ahakim <sambabdouhakim@gmail.com>
2026-08-21 16:48:18 +0000

    traduction fr

--- signin.php
+++ signin.php
@@ -3,11 +3,12 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 require_once(__DIR__ . "/php/Class/Admin.php");
+
 if (isset($_POST['conectee'])) {
     extract($_POST);
-    // recuperer le resultat d'admin authentifier 
+    // Récupérer le résultat de l'admin authentifié 
     $resultat = Admin::estAdmin($email, $mdp);
-    // var_dump($resultat);
+
     if (isset($resultat['email'])) {
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
@@ -20,17 +21,17 @@ if (isset($_POST['conectee'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <meta name="description" content="POS - Bootstrap Admin Template">
+    <meta name="description" content="POS - Modèle d'administration Bootstrap">
     <meta name="keywords"
-        content="admin, estimates, bootstrap, business, corporate, creative, invoice, html5, responsive, Projects">
-    <meta name="author" content="Dreamguys - Bootstrap Admin Template">
+        content="admin, estimations, bootstrap, entreprise, professionnel, créatif, facture, html5, responsive, projets">
+    <meta name="author" content="Dreamguys - Modèle d'administration Bootstrap">
     <meta name="robots" content="noindex, nofollow">
-    <title>AMITAM Store - Login</title>
+    <title>AMITAM Store - Connexion</title>
 
     <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
 
@@ -50,51 +51,49 @@ if (isset($_POST['conectee'])) {
                 <div class="login-content">
                     <form class="login-userset" method="post" action="./signin.php">
                         <div class="login-logo">
-                            <img src="assets/img/logo.png" alt="img">
+                            <img src="assets/img/logo.png" alt="logo">
                         </div>
                         <div class="login-userheading">
-                            <h3>Sign in</h3>
-                            <h4>Please login to your account</h4>
+                            <h3>Se connecter</h3>
+                            <h4>Veuillez vous connecter à votre compte</h4>
                         </div>
                         <div class="form-login">
-                            <label>Email</label>
+                            <label>Adresse e-mail</label>
                             <div class="form-addons">
-                                <input type="text" placeholder="Enter your email address" name="email">
-                                <img src="assets/img/icons/mail.svg" alt="img">
+                                <input type="text" placeholder="Entrez votre adresse e-mail" name="email">
+                                <img src="assets/img/icons/mail.svg" alt="icone-email">
                                 <?php if (isset($resultat)): ?>
-                                <?php if ($resultat === FAUX_EMAIL): ?>
-                                <p style="color:red; text-align: center">Invalid email</p>
-                                <?php endif ?>
+                                    <?php if ($resultat === FAUX_EMAIL): ?>
+                                        <p style="color:red; text-align: center">Adresse e-mail invalide</p>
+                                    <?php endif ?>
                                 <?php endif ?>
                             </div>
                         </div>
                         <div class="form-login">
-                            <label>Password</label>
+                            <label>Mot de passe</label>
                             <div class="pass-group">
-                                <input type="password" class="pass-input" placeholder="Enter your password" name="mdp">
+                                <input type="password" class="pass-input" placeholder="Entrez votre mot de passe" name="mdp">
                                 <span class="fas toggle-password fa-eye-slash"></span>
                             </div>
                             <div>
                                 <?php if (isset($resultat)): ?>
-                                <?php if ($resultat === FAUX_MDP): ?>
-                                <p style="color:red; text-align: center">Incorrect password</p>
-                                <?php endif ?>
+                                    <?php if ($resultat === FAUX_MDP): ?>
+                                        <p style="color:red; text-align: center">Mot de passe incorrect</p>
+                                    <?php endif ?>
                                 <?php endif ?>
                             </div>
                         </div>
                         <div class="form-login">
-                            <button class="btn btn-login" type="submit" name="conectee" value="conectee">Sign
-                                in</button>
+                            <button class="btn btn-login" type="submit" name="conectee" value="conectee">Se connecter</button>
                         </div>
                     </form>
                 </div>
                 <div class="login-img">
-                    <img src="assets/img/login.jpg" alt="img">
+                    <img src="assets/img/login.jpg" alt="image-connexion">
                 </div>
             </div>
         </div>
     </div>
-
 
     <script src="assets/js/jquery-3.6.0.min.js"></script>
 
