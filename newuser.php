b8b2d96f319569bc3ab6d73094bb1f72d78ce942
ahakim <sambabdouhakim@gmail.com>
2026-08-21 16:38:39 +0000

    traduction fr

--- newuser.php
+++ newuser.php
@@ -7,136 +7,136 @@ if (session_status() === PHP_SESSION_NONE) {
 <?php if (isset($_SESSION['admin'])):
     $active = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "active", 0);
 ?>
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="fr">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <meta name="description" content="POS - Bootstrap Admin Template">
-    <meta name="keywords"
-        content="admin, estimates, bootstrap, business, corporate, creative, invoice, html5, responsive, Projects">
-    <meta name="author" content="Dreamguys - Bootstrap Admin Template">
-    <meta name="robots" content="noindex, nofollow">
-    <title>New Admin</title>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <meta name="description" content="POS - Modèle d'administration Bootstrap">
+        <meta name="keywords"
+            content="admin, devis, bootstrap, entreprise, corporatif, créatif, facture, html5, responsive, projets">
+        <meta name="author" content="Dreamguys - Modèle d'administration Bootstrap">
+        <meta name="robots" content="noindex, nofollow">
+        <title>Nouvel Administrateur</title>
 
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+        <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
 
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 
-    <link rel="stylesheet" href="assets/css/animate.css">
+        <link rel="stylesheet" href="assets/css/animate.css">
 
-    <link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css">
+        <link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css">
 
-    <link rel="stylesheet" href="assets/css/dataTables.bootstrap4.min.css">
+        <link rel="stylesheet" href="assets/css/dataTables.bootstrap4.min.css">
 
-    <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
-    <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+        <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+        <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
 
-    <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/style.css">
 
-</head>
+    </head>
 
-<body>
-    <div id="global-loader">
-        <div class="whirly-loader"> </div>
-    </div>
+    <body>
+        <div id="global-loader">
+            <div class="whirly-loader"> </div>
+        </div>
 
-    <div class="main-wrapper">
+        <div class="main-wrapper">
 
-        <?php require_once("header.php"); ?>
-        <?php require_once("sidebar.php"); ?>
+            <?php require_once("header.php"); ?>
+            <?php require_once("sidebar.php"); ?>
 
 
-        <div class="page-wrapper">
-            <div class="content">
-                <div class="page-header">
-                    <div class="page-title">
-                        <h4>New Admin</h4>
-                        <h6>Create New Admin</h6>
+            <div class="page-wrapper">
+                <div class="content">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <h4>Nouvel Administrateur</h4>
+                            <h6>Créer un nouvel administrateur</h6>
+                        </div>
                     </div>
-                </div>
 
-                <form class="card" method="post" action="ajouterAdmin.php" enctype="multipart/form-data">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-lg-3 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>Family Name</label>
-                                    <input type="text" name="nom">
-                                </div>
-                                <div class="form-group">
-                                    <label>Address</label>
-                                    <input type="text" name="adr">
-                                </div>
-                                <div class="form-group">
-                                    <label>E-mail</label>
-                                    <div class="form-addons">
-                                        <input type="text" name="email" value="">
+                    <form class="card" method="post" action="ajouterAdmin.php" enctype="multipart/form-data">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-lg-3 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label>Nom</label>
+                                        <input type="text" name="nom">
                                     </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>First Name</label>
-                                    <input type="text" name="prenom">
-                                </div>
-                                <div class="form-group">
-                                    <label>Phone</label>
-                                    <input type="text" name="tele">
-                                </div>
-                                <div class="form-group">
-                                    <label>Password</label>
-                                    <div class="pass-group">
-                                        <input type="password" class=" pass-inputs" name="mdp" value="">
-                                        <span class="fas toggle-passworda fa-eye-slash"></span>
+                                    <div class="form-group">
+                                        <label>Adresse</label>
+                                        <input type="text" name="adr">
                                     </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label for="image"> Profile Picture</label>
-                                    <div class="image-upload image-upload-new">
-                                        <input type="file" id="image" name="image" accept="image/png, image/jpeg">
-                                        <div class="image-uploads">
-                                            <img src="assets/img/icons/upload.svg" alt="img">
-                                            <h4>Drag and drop a file to upload</h4>
+                                    <div class="form-group">
+                                        <label>E-mail</label>
+                                        <div class="form-addons">
+                                            <input type="text" name="email" value="">
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-lg-12">
-                                <button class="btn btn-submit me-2" type="submit" name="ajouter">Add</button>
-                                <button class="btn btn-cancel" type="reset">Cancel</button>
+                                <div class="col-lg-3 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label>Prénom</label>
+                                        <input type="text" name="prenom">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Téléphone</label>
+                                        <input type="text" name="tele">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Mot de passe</label>
+                                        <div class="pass-group">
+                                            <input type="password" class=" pass-inputs" name="mdp" value="">
+                                            <span class="fas toggle-passworda fa-eye-slash"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label for="image"> Photo de profil</label>
+                                        <div class="image-upload image-upload-new">
+                                            <input type="file" id="image" name="image" accept="image/png, image/jpeg">
+                                            <div class="image-uploads">
+                                                <img src="assets/img/icons/upload.svg" alt="img">
+                                                <h4>Glissez-déposez un fichier pour le téléverser</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <button class="btn btn-submit me-2" type="submit" name="ajouter">Ajouter</button>
+                                    <button class="btn btn-cancel" type="reset">Annuler</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
 
 
-    <script src="assets/js/jquery-3.6.0.min.js"></script>
+        <script src="assets/js/jquery-3.6.0.min.js"></script>
 
-    <script src="assets/js/feather.min.js"></script>
+        <script src="assets/js/feather.min.js"></script>
 
-    <script src="assets/js/jquery.slimscroll.min.js"></script>
+        <script src="assets/js/jquery.slimscroll.min.js"></script>
 
-    <script src="assets/js/jquery.dataTables.min.js"></script>
-    <script src="assets/js/dataTables.bootstrap4.min.js"></script>
+        <script src="assets/js/jquery.dataTables.min.js"></script>
+        <script src="assets/js/dataTables.bootstrap4.min.js"></script>
 
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
 
-    <script src="assets/plugins/select2/js/select2.min.js"></script>
+        <script src="assets/plugins/select2/js/select2.min.js"></script>
 
-    <script src="assets/plugins/sweetalert/sweetalert2.all.min.js"></script>
-    <script src="assets/plugins/sweetalert/sweetalerts.min.js"></script>
+        <script src="assets/plugins/sweetalert/sweetalert2.all.min.js"></script>
+        <script src="assets/plugins/sweetalert/sweetalerts.min.js"></script>
 
-    <script src="assets/js/script.js"></script>
-</body>
+        <script src="assets/js/script.js"></script>
+    </body>
 
-</html>
+    </html>
 <?php else: ?>
-<?php header("Location: signin.php"); ?>
+    <?php header("Location: signin.php"); ?>
 <?php endif ?>
