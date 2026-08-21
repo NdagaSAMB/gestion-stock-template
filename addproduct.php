c0d641451787411efe16880599d7c2dc6e68ee7c
ahakim <sambabdouhakim@gmail.com>
2026-08-21 15:41:01 +0000

    traduction fr

--- addproduct.php
+++ addproduct.php
@@ -1,10 +1,10 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) {
-    session_start();
+  session_start();
 }
 ?>
 <?php if (isset($_SESSION['admin'])): ?>
-<?php
+  <?php
   require_once(__DIR__ . "/php/Class/Product.php");
   require_once(__DIR__ . "/php/Class/Categorie.php");
   require_once(__DIR__ . "/php/Class/Marque.php");
@@ -32,196 +32,194 @@ if (session_status() === PHP_SESSION_NONE) {
         Stock::addMouvement($num_pr, Stock::ENTREE_INITIALE, $qte_stock, $num_pr, "Stock initial à la création du produit");
       }
     } else {
-      exit("<h3> Failed to upload image!</h3>");
+      exit("<h3>Échec de l'envoi de l'image !</h3>");
     }
   }
-?>
-<!DOCTYPE html>
-<html lang="en">
+  ?>
+  <!DOCTYPE html>
+  <html lang="fr">
 
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
-  <meta name="description" content="POS - Bootstrap Admin Template" />
-  <meta name="keywords"
-    content="admin, estimates, bootstrap, business, corporate, creative, invoice, html5, responsive, Projects" />
-  <meta name="author" content="Dreamguys - Bootstrap Admin Template" />
-  <meta name="robots" content="noindex, nofollow" />
-  <title>Add Product</title>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
+    <meta name="description" content="POS - Modèle d'administration Bootstrap" />
+    <meta name="keywords"
+      content="admin, estimations, bootstrap, business, corporate, creative, facture, html5, responsive, Projets" />
+    <meta name="author" content="Dreamguys - Modèle d'administration Bootstrap" />
+    <meta name="robots" content="noindex, nofollow" />
+    <title>Ajouter un Produit</title>
 
-  <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png" />
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png" />
 
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
 
-  <link rel="stylesheet" href="assets/css/animate.css" />
+    <link rel="stylesheet" href="assets/css/animate.css" />
 
-  <link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css" />
+    <link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css" />
 
-  <link rel="stylesheet" href="assets/css/dataTables.bootstrap4.min.css" />
+    <link rel="stylesheet" href="assets/css/dataTables.bootstrap4.min.css" />
 
-  <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css" />
-  <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css" />
+    <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css" />
+    <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css" />
 
-  <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+  </head>
 
-  </style>
-</head>
+  <body>
+    <div id="global-loader">
+      <div class="whirly-loader"></div>
+    </div>
 
-<body>
-  <div id="global-loader">
-    <div class="whirly-loader"></div>
-  </div>
-
-  <div class="main-wrapper">
-    <?php require_once("header.php"); ?>
-    <?php require_once("sidebar.php"); ?>
-    <div class="page-wrapper">
-      <div class="content">
-        <div class="page-header">
-          <div class="page-title">
-            <h4>Add Product</h4>
-            <h6>Create new product</h6>
+    <div class="main-wrapper">
+      <?php require_once("header.php"); ?>
+      <?php require_once("sidebar.php"); ?>
+      <div class="page-wrapper">
+        <div class="content">
+          <div class="page-header">
+            <div class="page-title">
+              <h4>Ajouter un Produit</h4>
+              <h6>Créer un nouveau produit</h6>
+            </div>
           </div>
-        </div>
-        <div class="card">
-          <div class="card-body">
-            <form class="row" method="post" action="addproduct.php" enctype="multipart/form-data">
-              <div class="col-lg-3 col-sm-6 col-12">
-                <div class="form-group">
-                  <label>Reference</label>
-                  <input type="text" name="num_pr" />
+          <div class="card">
+            <div class="card-body">
+              <form class="row" method="post" action="addproduct.php" enctype="multipart/form-data">
+                <div class="col-lg-3 col-sm-6 col-12">
+                  <div class="form-group">
+                    <label>Référence</label>
+                    <input type="text" name="num_pr" />
+                  </div>
                 </div>
-              </div>
-              <div class="col-lg-3 col-sm-6 col-12">
-                <div class="form-group">
-                  <label>Product Name</label>
-                  <input type="text" name="lib_pr" />
+                <div class="col-lg-3 col-sm-6 col-12">
+                  <div class="form-group">
+                    <label>Nom du produit</label>
+                    <input type="text" name="lib_pr" />
+                  </div>
                 </div>
-              </div>
-              <div class="col-lg-3 col-sm-6 col-12">
-                <div class="form-group">
-                  <label>Category</label>
-                  <select class="select" name="id_cat">
-                    <option value="">Choose Category</option>
-                    <?php foreach ($cats as $item): ?>
-                    <option value="<?= $item['id_cat']; ?>">
-                      <?= $item['lib_cat']; ?>
-                    </option>
-                    <?php endforeach ?>
-                  </select>
+                <div class="col-lg-3 col-sm-6 col-12">
+                  <div class="form-group">
+                    <label>Catégorie</label>
+                    <select class="select" name="id_cat">
+                      <option value="">Choisir une catégorie</option>
+                      <?php foreach ($cats as $item): ?>
+                        <option value="<?= $item['id_cat']; ?>">
+                          <?= $item['lib_cat']; ?>
+                        </option>
+                      <?php endforeach ?>
+                    </select>
+                  </div>
                 </div>
-              </div>
-              <div class="col-lg-3 col-sm-6 col-12">
-                <div class="form-group">
-                  <label>Brand</label>
-                  <select class="select" name="id_marque">
-                    <option value="">Choose Brand</option>
-                    <?php foreach ($brands as $item): ?>
-                    <option value="<?= $item['id_marque']; ?>">
-                      <?= $item['nom_marque']; ?>
-                    </option>
-                    <?php endforeach ?>
-                  </select>
+                <div class="col-lg-3 col-sm-6 col-12">
+                  <div class="form-group">
+                    <label>Marque</label>
+                    <select class="select" name="id_marque">
+                      <option value="">Choisir une marque</option>
+                      <?php foreach ($brands as $item): ?>
+                        <option value="<?= $item['id_marque']; ?>">
+                          <?= $item['nom_marque']; ?>
+                        </option>
+                      <?php endforeach ?>
+                    </select>
+                  </div>
                 </div>
-              </div>
-              <div class="col-lg-3 col-sm-6 col-12">
-                <div class="form-group">
-                  <label>Quantity</label>
-                  <input type="text" name="qte_stock" />
+                <div class="col-lg-3 col-sm-6 col-12">
+                  <div class="form-group">
+                    <label>Quantité</label>
+                    <input type="text" name="qte_stock" />
+                  </div>
                 </div>
-              </div>
-              <div class="col-lg-3 col-sm-6 col-12">
-                <div class="form-group">
-                  <label>purchase price</label>
-                  <input type="text" name="prix_achat" />
+                <div class="col-lg-3 col-sm-6 col-12">
+                  <div class="form-group">
+                    <label>Prix d'achat</label>
+                    <input type="text" name="prix_achat" />
+                  </div>
                 </div>
-              </div>
-              <div class="col-lg-3 col-sm-6 col-12">
-                <div class="form-group">
-                  <label>unit price</label>
-                  <input type="text" name="prix_uni" />
+                <div class="col-lg-3 col-sm-6 col-12">
+                  <div class="form-group">
+                    <label>Prix unitaire</label>
+                    <input type="text" name="prix_uni" />
+                  </div>
                 </div>
-              </div>
 
-              <div class="col-lg-3 col-sm-6 col-12">
-                <div class="form-group">
-                  <label>description</label>
-                  <input type="text" name="desc_pr" />
+                <div class="col-lg-3 col-sm-6 col-12">
+                  <div class="form-group">
+                    <label>Description</label>
+                    <input type="text" name="desc_pr" />
+                  </div>
                 </div>
-              </div>
-              <div class="col-lg-3 col-sm-6 col-12">
-                <div class="form-group">
-                  <label>Seuil de stock minimum</label>
-                  <input type="text" name="seuil_min" placeholder="5" />
+                <div class="col-lg-3 col-sm-6 col-12">
+                  <div class="form-group">
+                    <label>Seuil de stock minimum</label>
+                    <input type="text" name="seuil_min" placeholder="5" />
+                  </div>
                 </div>
-              </div>
-              <div class="col-lg-3 col-sm-6 col-12">
-                <div class="form-group">
-                  <label>Seuil de stock maximum</label>
-                  <input type="text" name="seuil_max" placeholder="100" />
+                <div class="col-lg-3 col-sm-6 col-12">
+                  <div class="form-group">
+                    <label>Seuil de stock maximum</label>
+                    <input type="text" name="seuil_max" placeholder="100" />
+                  </div>
                 </div>
-              </div>
-              <div class="col-lg-3 col-sm-6 col-12">
-                <div class="form-group">
-                  <label>Code-barres / QR code</label>
-                  <input type="text" name="code_barre" placeholder="Optionnel" />
+                <div class="col-lg-3 col-sm-6 col-12">
+                  <div class="form-group">
+                    <label>Code-barres / QR code</label>
+                    <input type="text" name="code_barre" placeholder="Optionnel" />
+                  </div>
                 </div>
-              </div>
-              <div class="col-lg-3 col-sm-6 col-12">
-                <div class="form-group">
-                  <label>Unité de mesure</label>
-                  <input type="text" name="unite_mesure" placeholder="pièce" />
+                <div class="col-lg-3 col-sm-6 col-12">
+                  <div class="form-group">
+                    <label>Unité de mesure</label>
+                    <input type="text" name="unite_mesure" placeholder="pièce" />
+                  </div>
                 </div>
-              </div>
-              <div class="col-lg-3 col-sm-6 col-12">
-                <div class="form-group">
-                  <label>Date de péremption</label>
-                  <input type="date" name="date_peremption" />
+                <div class="col-lg-3 col-sm-6 col-12">
+                  <div class="form-group">
+                    <label>Date de péremption</label>
+                    <input type="date" name="date_peremption" />
+                  </div>
                 </div>
-              </div>
-              <div class="col-lg-12">
-                <div class="form-group">
-                  <label> Product Image</label>
-                  <div class="image-upload">
-                    <input type="file" name="image" />
-                    <div class="image-uploads">
-                      <img src="assets/img/icons/upload.svg" alt="img" />
-                      <h4>Drag and drop a file to upload</h4>
+                <div class="col-lg-12">
+                  <div class="form-group">
+                    <label>Image du produit</label>
+                    <div class="image-upload">
+                      <input type="file" name="image" />
+                      <div class="image-uploads">
+                        <img src="assets/img/icons/upload.svg" alt="img" />
+                        <h4>Glissez-déposez un fichier à téléverser</h4>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div class="col-lg-12">
-                <button class="btn btn-submit me-2" name="add">Add product</button>
-                <a href="productlist.php" class="btn btn-cancel">Cancel</a>
-              </div>
-            </form>
+                <div class="col-lg-12">
+                  <button class="btn btn-submit me-2" name="add">Ajouter le produit</button>
+                  <a href="productlist.php" class="btn btn-cancel">Annuler</a>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
 
-  <script src="assets/js/feather.min.js"></script>
+    <script src="assets/js/feather.min.js"></script>
 
-  <script src="assets/js/jquery.slimscroll.min.js"></script>
+    <script src="assets/js/jquery.slimscroll.min.js"></script>
 
-  <script src="assets/js/jquery.dataTables.min.js"></script>
-  <script src="assets/js/dataTables.bootstrap4.min.js"></script>
+    <script src="assets/js/jquery.dataTables.min.js"></script>
+    <script src="assets/js/dataTables.bootstrap4.min.js"></script>
 
-  <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
 
-  <script src="assets/plugins/select2/js/select2.min.js"></script>
+    <script src="assets/plugins/select2/js/select2.min.js"></script>
 
-  <script src="assets/plugins/sweetalert/sweetalert2.all.min.js"></script>
-  <script src="assets/plugins/sweetalert/sweetalerts.min.js"></script>
+    <script src="assets/plugins/sweetalert/sweetalert2.all.min.js"></script>
+    <script src="assets/plugins/sweetalert/sweetalerts.min.js"></script>
 
-  <script src="assets/js/script.js"></script>
-</body>
+    <script src="assets/js/script.js"></script>
+  </body>
 
-</html>
+  </html>
 <?php else: ?>
-<?php header("Location: signin.php"); ?>
+  <?php header("Location: signin.php"); ?>
 <?php endif ?>
